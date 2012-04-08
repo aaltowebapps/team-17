16254e247975f5e059f1da8eaa13f1abd1eef707
Emil S <emil.sagfors@aalto.fi>
2012-04-08 20:38:26 +0300

    show duration as HH:MM:SS

--- public/js/reittiopas.js
+++ public/js/reittiopas.js
@@ -13,7 +13,7 @@ function getRoutes(fromX, fromY, toX, toY, show) {
       show: show
     }, function(json) {
       var content = Templates.routes(json);
-      $("#routesList").html(content).find("ul").listview();
+      $("#routesList").html(content).trigger('create');
   });
 }
 
@@ -21,6 +21,15 @@ $(function() {
 
   $('script[type="text/x-handlebars-template"]').each(function () {
     Templates[this.id] = Handlebars.compile($(this).html());
+  });
+
+  Handlebars.registerHelper('durationHMS', function() {
+    // seconds to hh:mm:ss, from http://snipplr.com/view.php?codeview&id=20348
+    d = Number(this.duration);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s);
   });
 
   getRoutes(2561133,6699755,2527815,6662705,5);
